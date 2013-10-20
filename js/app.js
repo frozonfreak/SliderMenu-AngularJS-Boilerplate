d235ddbe714f6491581f504979ceb471ccafcb46
Sastha K L <shazcool@gmail.com>
2013-10-20 15:34:33 +0800

    update partials

--- js/app.js
+++ js/app.js
@@ -9,12 +9,37 @@ smApp.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: "partials/home.html",
       controller: 'appController',
     })
+    .state('view', {
+      url: "/view",
+      templateUrl: "partials/view2.html",
+      controller: 'appViewController',
+    })
     // For any unmatched url, redirect to /state1
      $urlRouterProvider.otherwise("/404");
 
 });
 //controller
 smApp.controller('appController', function($scope){
+
+	$scope.alerts = [];
+	$scope.searchText;
+	
+	$scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };	
+  	//Initializer
+	init();
+	function init(){
+		
+		
+	};
+	$scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+	
+});
+//controller
+smApp.controller('appViewController', function($scope){
 
 	$scope.alerts = [];
 	$scope.searchText;
